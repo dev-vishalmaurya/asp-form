593f7fad1e8a93497b512768933228eabc4440d3
Vishal Maurya <vishalmaurya@gmail.com>
2023-06-13 16:21:41 +0530

    First asp.net form page

---
+++ Default.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <main>            
+    <div class="row mx-auto w-50">
+    <asp:Label ID="Label1" runat="server" Text="Name"></asp:Label>
+    <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+
+        <asp:Label ID="Label2" runat="server" Text="EmailId"></asp:Label>
+      <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+
+
+        <asp:Label ID="Label3" runat="server" Text="Address"></asp:Label>
+        <asp:TextBox ID="txtAddress" runat="server"></asp:TextBox>
+
+
+        <asp:Label ID="Label4" runat="server" Text="Pincode"></asp:Label>
+        <asp:TextBox ID="txtPinCode" runat="server"></asp:TextBox>
+        <asp:Label ID="Label6" runat="server" Text="Gender"></asp:Label>
+        <asp:DropDownList ID="txtGender" runat="server">
+            <asp:ListItem>Female</asp:ListItem>
+            <asp:ListItem>other</asp:ListItem>
+            
+            <asp:ListItem Selected="True">Male</asp:ListItem>
+            
+        </asp:DropDownList>
+        
+
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:OnlineShopCon %>" ProviderName="<%$ ConnectionStrings:OnlineShopCon.ProviderName %>" SelectCommand="SELECT * FROM [tblUserSignUp]"></asp:SqlDataSource>
+        
+
+        <asp:Label ID="Label5" runat="server" Text="Password"></asp:Label>
+        <asp:TextBox ID="txtPassword" runat="server" TextMode="Password"></asp:TextBox>
+
+        <asp:Button ID="Button1" runat="server" Text="Submit" CssClass="mt-2" OnClick="Button1_Click" />
+        
+
+    </div>
+    </main>
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+
+        {
+            registration();
+            //Response.Write("<script>alert('Success')</script>");
+
+        }
+        public void registration()
+        {
+            try
+            {
+                string s = @"Data Source=LAPTOP-CQ57ICGG\SQLEXPRESS;Initial Catalog=ShoppingApp;Integrated Security=True";
+                SqlConnection con = new SqlConnection(s);
+                //con.ConnectionString = s;
+                SqlCommand cmd = new SqlCommand("insert into tblshoplogin (Name,EmailId,Address,PinCode,Gender,Password)values(@Name,@EmailId,@Address,@PinCode,@Gender,@Password)", con);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                cmd.Parameters.AddWithValue("@Name", txtName.Text);
+                cmd.Parameters.AddWithValue("@EmailId", txtEmail.Text);
+                cmd.Parameters.AddWithValue("@Address", txtAddress.Text);
+                cmd.Parameters.AddWithValue("@PinCode", txtPinCode.Text);
+                cmd.Parameters.AddWithValue("@Gender", txtGender.Text);
+                cmd.Parameters.AddWithValue("@Password", txtPassword.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                ClearInput();
+                Response.Write("<script>alert('Success')</script>");
+
+                //ClearInput();
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('"+ ex.Message+"')</script>");
+
+            }
+        }
+        public void ClearInput()
+        {
+            txtName.Text = string.Empty;
+            txtEmail.Text = string.Empty;
+            txtAddress.Text = string.Empty;
+                txtPinCode.Text = string.Empty;
+            txtGender.Text = string.Empty;
+            txtPassword.Text = string.Empty;
+            
+        }
+
+        
+    }
+}
